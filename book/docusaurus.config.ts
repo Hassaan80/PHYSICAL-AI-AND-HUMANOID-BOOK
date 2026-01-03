6d4bb61ccf4132b359ae074a42e5428c5cc93537
Hassaan80 <m.hassanzsky@gmail.com>
2026-01-03 14:08:43 +0500

    Update docusaurus.config.ts

--- book/docusaurus.config.ts
+++ book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://physical-al-and-humanoid-book-hplkb0o8k.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
